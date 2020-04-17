19d98472c8111ffcba612336eab4c369a3848c3e
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-04-16 19:26:14 -0700

    render all 3 services to the page

--- index.js
+++ index.js
@@ -8,19 +8,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/songs/:songId", async (req, res) => {
-  const { locationId } = req.params;
+  const { songId } = req.params;
   await axios
     .get(
-      `http://ec2-54-219-53-4.us-west-1.compute.amazonaws.com/`
+      `http://ec2-54-219-53-4.us-west-1.compute.amazonaws.com/songs/${songId}`
     )
-    .then(result => {
+    .then((result) => {
       res.send(result.data);
     });
   res.end();
 });
 
+app.get("/getPlaylist/:playlist_Id", async (req, res) => {
+  const { playlist_Id } = req.params;
 
+  await axios
+    .get(
+      `http://ec2-54-183-39-164.us-west-1.compute.amazonaws.com/getPlaylist/${playlist_Id}`
+    )
+    .then((result) => {
+      res.send(result.data);
+    });
+  res.end();
+});
 
+app.get("/artist", async (req, res) => {
+  const { playlist_Id } = req.params;
+  await axios
+    .get(
+      `http://ec2-54-183-200-194.us-west-1.compute.amazonaws.com/artist`
+    )
+    .then((result) => {
+      res.send(result.data);
+    });
+  res.end();
+});
 
 app.listen(4040, () => {
   console.log("Listening on 4040");
